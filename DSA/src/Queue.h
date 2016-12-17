87c2262ebdc33e8a86f04a04481679d765ddf168
qyb225 <qyb225@gmail.com>
2016-12-17 22:19:03 +0800

    Fix some bugs

--- DSA/src/Queue.h
+++ DSA/src/Queue.h
@@ -5,7 +5,7 @@
 template <class T>
 class Queue : public List<T> {
 public: //size(), empty();
-    void enqueue(const T& e) { insertAsFirst(e); }
+    void enqueue(const T& e) { insertAsLast(e); }
     T dequeue() { return remove(first()); }
     T& front() { return first() -> data; }
 };
