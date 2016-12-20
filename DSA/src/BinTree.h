90f3d1a2d257e8bebac1267ebcfd9f2f19d4ff58
qyb225 <qyb225@gmail.com>
2016-12-20 11:44:23 +0800

    Update

--- DSA/src/BinTree.h
+++ DSA/src/BinTree.h
@@ -2,6 +2,7 @@
 #define BINTREE_H
 #include "Stack.h"
 #include "Queue.h"
+#include <iostream>
 template <class T>
 struct BinNode {
     BinNode<T>* parent, *lChild, *rChild;
@@ -11,11 +12,12 @@ struct BinNode {
     void insertAsRC(BinNode<T>*);
     BinNode<T>* succ();
     int size();
-    BinNode() { parent = NULL; lChild = NULL; rChild = NULL; height = 0; }
-    BinNode(const T& d, BinNode<T>* p = NULL, BinNode<T>* l = NULL, BinNode<T>* r = NULL)
-        : data(d), parent(p), lChild(l), rChild(r) {
-        height = 0;
-    }
+    BinNode()
+        : parent(NULL), lChild(NULL), rChild(NULL), height(0), __size(1) {}
+    BinNode(const T& d)
+        : data(d), parent(NULL), lChild(NULL), rChild(NULL), height(0), __size(1) {}
+    BinNode(const T& d, int h, int s)
+        : data(d), height(h), __size(s), parent(NULL), lChild(NULL), rChild(NULL) {}
     template <class VST> void traveLevel(VST &);
     template <class VST> void travePre_1(VST &);
     template <class VST> void travePre_2(VST &);
@@ -31,23 +33,31 @@ private:
 protected:
     virtual int updateHeight(BinNode<T>*);
     void updateHeightAbove(BinNode<T>*);
+    BinNode<T>* copyNode(const BinNode<T>*);
 public:
     BinTree() { _size = 0; _root = NULL; }
-    BinTree(T &);
-    //BinTree(BinTree<T>&);
+    BinTree(const T &);
+    BinTree(BinNode<T>*);
+    BinTree(const BinTree<T>&);
     void creatRoot(const T&);
     int size() const { return _size; }
-    bool empty() { return !_root; }
+    bool empty() { return _size == 0; }
     BinNode<T>* root() { return _root; }
     int height() { return _root -> height; }
-    BinNode<T>* insertAsLC(const T& e, BinNode<T>* x);
-    BinNode<T>* insertAsRC(const T& e, BinNode<T>* x);
-    T deleteNode(BinNode<T>*);
+    BinTree<T>& secede(BinNode<T>*);
+    BinTree<T>& secede(BinNode<T>*, BinTree<T>&);
+    BinNode<T>* insertAsLC(const T&, BinNode<T>*);
+    BinNode<T>* insertAsRC(const T&, BinNode<T>*);
+    BinNode<T>* attachAsLC(BinNode<T>*, BinNode<T>*); //insert a node(subtree) as left child.
+    BinNode<T>* attachAsRC(BinNode<T>*, BinNode<T>*);
+    BinNode<T>* find(T, BinNode<T>*); //Find a node in a subtree by TravePre.
+    BinNode<T>* find(T);
+    int remove(BinNode<T>*);
     template <class VST> void travePre(VST & visit) { _root -> travePre_1(visit); }
     template <class VST> void traveIn(VST & visit) { _root -> traveIn(visit); }
     template <class VST> void travePost(VST & visit) { _root -> travePost(visit); }
     template <class VST> void traveLevel(VST & visit) { _root -> traveLevel(visit); }
-    ~BinTree() { deleteNode(_root); }
+    ~BinTree() { if (_size > 0) remove(_root); }
 };
 
 template <class T, class VST>
@@ -69,12 +79,14 @@ template <class T>
 void BinNode<T>::insertAsLC(BinNode<T>* l) {
     lChild = l;
     l -> parent = this;
+    __size += l -> __size;
 }
 
 template <class T>
 void BinNode<T>::insertAsRC(BinNode<T>* r) {
     rChild = r;
     r -> parent = this;
+    __size += r -> __size;
 }
 
 //1.1 travelPre
@@ -145,17 +157,30 @@ void BinNode<T>::traveLevel(VST& visit) {
 
 //2. BinTree
 template <class T>
-BinTree<T>::BinTree(T& e) {
+BinTree<T>::BinTree(const T& e) {
     _root = new BinNode<T>(e);
     _size = 1;
 }
 
 template <class T>
-void BinTree<T>::creatRoot(const T& e) {
-    if (empty()) {
-        _root = new BinNode<T>(e);
-        _size = 1;
+BinTree<T>::BinTree(const BinTree<T>& temp) {
+    _size = temp._size;
+    _root = copyNode(temp._root);
+}
+
+template <class T>
+BinTree<T>::BinTree(BinNode<T>* r): _size(0), _root(NULL) {
+    if (!(r -> parent)) {
+        _size = r -> size();
+        _root = r;
     }
+}
+
+template <class T>
+void BinTree<T>::creatRoot(const T& e) {
+    if (!empty()) remove(_root);
+    _root = new BinNode<T>(e);
+    _size = 1;
 }
 
 template <class T>
@@ -187,6 +212,7 @@ BinNode<T>* BinTree<T>::insertAsLC(const T& e, BinNode<T>* x) {
     BinNode<T>* n = new BinNode<T>(e);
     x -> insertAsLC(n);
     ++_size;
+    updateHeightAbove(x);
     return n;
 }
 
@@ -200,13 +226,95 @@ BinNode<T>* BinTree<T>::insertAsRC(const T& e, BinNode<T>* x) {
 }
 
 template <class T>
-T BinTree<T>::deleteNode(BinNode<T>* x) {
-    if (x -> lChild) deleteNode(x -> lChild);
-    if (x -> rChild) deleteNode(x -> rChild);
-    T temp = x -> data;
+int BinTree<T>::remove(BinNode<T>* x) {
+    if (!x) return 0;
+    int temp = 1;
+    if (x -> lChild) temp += remove(x -> lChild);
+    if (x -> rChild) temp += remove(x -> rChild);
     delete x;
+    x = NULL;
     --_size;
     return temp;
+}
+
+template <class T>
+BinNode<T>* BinTree<T>::find(T e, BinNode<T>* x) {
+    Stack<BinNode<T>*> s;
+    s.push(x);
+    while (!s.empty()) {
+        x = s.pop();
+        if (e == x -> data) return x;
+        if (x -> rChild) s.push(x -> rChild);
+        if (x -> lChild) s.push(x -> lChild);
+    }
+}
+
+template <class T>
+BinNode<T>* BinTree<T>::find(T e) {
+    return find(e, _root);
+}
+
+template <class T>
+BinNode<T>* BinTree<T>::copyNode(const BinNode<T>* x) {
+    BinNode<T>* r = new BinNode<T>(x -> data, x -> height, x -> __size);
+    if (x -> lChild) {
+        r -> lChild = copyNode(x -> lChild);
+        r -> lChild -> parent = r;
+    }
+    if (x -> rChild) {
+        r -> rChild = copyNode(x -> rChild);
+        r -> rChild -> parent = r;
+    }
+    r -> size();
+    return r;
+}
+
+template <class T>
+BinNode<T>* BinTree<T>::attachAsLC(BinNode<T>* e, BinNode<T>* x) {
+    if (x -> lChild) remove(x -> lChild);
+    e -> size();
+    x -> insertAsLC(copyNode(e));
+    updateHeightAbove(x);
+    _size += x -> __size - 1;
+    return x -> lChild;
+}
+
+template <class T>
+BinNode<T>* BinTree<T>::attachAsRC(BinNode<T>* e, BinNode<T>* x) {
+    if (x -> rChild) remove(x -> rChild);
+    e -> size();
+    x -> insertAsRC(copyNode(e));
+    updateHeightAbove(x);
+    _size += x -> __size - 1;
+    return x -> rChild;
+}
+
+template <class T>
+BinTree<T>& BinTree<T>::secede(BinNode<T>* x) {
+    if (x -> parent && x -> parent -> lChild == x)
+        x -> parent -> lChild = NULL;
+    else if (x -> parent && x -> parent -> rChild == x)
+        x -> parent -> rChild = NULL;
+    updateHeightAbove(x -> parent);
+    x -> parent = NULL;
+    remove(_root);
+    _size = x -> size();
+    _root = x;
+    return *this;
+}
+
+template <class T>
+BinTree<T>& BinTree<T>::secede(BinNode<T>* x, BinTree<T>& t) {
+    if (x -> parent && x -> parent -> lChild == x)
+        x -> parent -> lChild = NULL;
+    else if (x -> parent && x -> parent -> rChild == x)
+        x -> parent -> rChild = NULL;
+    updateHeightAbove(x -> parent);
+    x -> parent = NULL;
+    remove(t._root);
+    t._size = x -> size();
+    t._root = x;
+    return t;
 }
 
 //3.Others
--- README.md
+++ README.md
@@ -4,3 +4,32 @@
 
 1. Cpp：C++语法。
 2. DSA：Data Structure & Algorithm 数据结构与算法
+
+---
+
+##Tips: 内存泄漏检测Debug方法：
+
+```cpp
+#define CRTDBG_MAP_ALLOC
+#include <cstdlib>
+#include <crtdbg.h>
+//Memory leaks test in VS.
+//If memory doesn't release finally, will output warn like:
+/*
+Detected memory leaks!
+normal block at 0x00341A38, 40 bytes long.
+Data: <   > CD CD CD CD CD CD CD CD CD CD
+*/
+
+void func();
+int main() {
+    func();
+    _CrtDumpMemoryLeaks();
+    return 0;
+}
+
+void func() {
+    int* p = new int[10];
+    //delete[] p;
+}
+```
