775358df640d0636f1a5587393a91f8103f099b2
qyb225 <qyb225@gmail.com>
2016-12-26 15:46:21 +0800

    Update problems

---
+++ DSA/src/DS_problems/stack_app.h
@@ -0,0 +1,13 @@
+#include <string>
+#include "../DS_header/Stack.h"
+using namespace std;
+
+bool paren(string exp); //Brace matching(only "()").
+string conversion(int num, int base); //Conversion e.g. 31(10) -> 1F(16).
+
+char optr_cmp(char a, char b);
+bool is_digit(char a);
+double calculate_express(char* express);
+
+template <class T>
+bool if_stack_mix(T * a, T * b, int n);
