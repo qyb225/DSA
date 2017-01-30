f1e0ad5f439edc1b827b8672ccebf5cb8d4604ea
qyb225 <qyb225@gmail.com>
2017-01-30 10:51:46 +0800

    BTree search

--- DSA/inc/Tree/BTree.h
+++ DSA/inc/Tree/BTree.h
@@ -26,6 +26,24 @@ protected:
     BTNode<T>* _hot; //search()访问最后非空节点位置
     void solveOverflow(BTNode<T>*);  //因插入而上溢的 分裂 处理
     void solveUnderflow(BTNode<T>*); //因删除而下溢的 合并 处理
+public:
+    BTNode<T>* search(const T&);
+    bool insert(const T&);
+    bool remove(const T&);
 };
+
+template <class T>
+BTNode<T>* BTree<T>::search(const T& e) {
+    BTNode<T>* v = _root;
+    _hot = NULL;
+    while (v) {
+        int r = v->key.search(e);
+        if (r > -1 && v->key[r] == e)
+            return v;
+        _hot = v;
+        v = v->child[r + 1];
+    }
+    return NULL;
+}
 
 #endif
